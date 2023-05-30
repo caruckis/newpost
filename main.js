b61cb4789b2f719d92e94607e936e9df879f36d8
caruckis <131708329+caruckis@users.noreply.github.com>
2023-05-30 18:01:17 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,23 +1,14 @@
 var btn = document.querySelector(".no");
 var position
+btn.addEventListener("click", function() {
+position ? (position = 0) : (position =     150);
+btn.style.transform =    `translate(${position}px,0px)`;
+btn.style.transition = "all 0.3s ease";
+});
+function tanks() {
+    
 
-
-if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-    btn.addEventListener("touchstart", function() {
-        position ? (position = 0) : (position = 150);
-        btn.style.transform = `translate(${position}px,0px)`;
-        btn.style.transition = "all 0.3s ease";
-      });
-      
-      btn.addEventListener("touchend", function() {
-        position ? (position = 0) : (position = 150);
-        btn.style.transform = `translate(${position}px,0px)`;
-        btn.style.transition = "all 0.3s ease";
-      });
-  } else {
-    btn.addEventListener("mouseover", function() {
-        position ? (position = 0) : (position = 150);
-        btn.style.transform = `translate(${position}px,0px)`;
-        btn.style.transition = "all 0.3s ease";
-        });
-  }
+}  
+function not() {
+document.getElementsByClassName('no').style.translateX = ("150px");
+}
